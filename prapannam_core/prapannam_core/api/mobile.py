0069dc9039ad95dabd64b480405ecb33f3d7e5bf
Pushakar Gaikwad <pushakargaikwad@gmail.com>
2024-10-14 07:12:45 +0000

    updated variable for future features

--- prapannam_core/prapannam_core/api/mobile.py
+++ prapannam_core/prapannam_core/api/mobile.py
@@ -3,9 +3,10 @@ import frappe
 # Get Details of current user
 @frappe.whitelist()
 def get_current_user():
-    current_user = frappe.session.user
+    current_user_name = frappe.session.user
     
     return {
         "status": "ok",
-        "user": current_user,
+        "user": current_user_name,
+
     }
